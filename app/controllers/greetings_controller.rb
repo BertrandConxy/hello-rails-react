f588f73b8b0b7614684370c414066331fee22358
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-06-07 17:00:00 +0200

    setup redux

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,6 +1,6 @@
 class GreetingsController < ApplicationController
   def show
     random_id = rand 1..Greeting.count
-    @greeting = Greeting.find( random_id )
+    @greeting = Greeting.find(random_id)
   end
 end
--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,15 +1,32 @@
-import React from "react"
+import React, {useEffect} from "react"
+import { useSelector, useDispatch } from "react-redux";
+import {loadGreetings} from "../redux/greetings";
 import PropTypes from "prop-types"
-class Greeting extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-        Greeting: {this.props.greeting}
-      </React.Fragment>
-    );
-  }
-}
+const Greeting = () => {
+    const dispatch = useDispatch();
+    useEffect(()=> {
+      dispatch(loadGreetings());
+    },[])
+const greeting = useSelector((state) => state.greetings);
 
+    if(greeting.data) {
+        return (
+          <>
+            <h1>{greeting.data.text}</h1>
+            <button onClick={() => dispatch(loadGreetings())}>
+              Next message
+            </button>
+          </>
+        );
+    } else {
+        return (
+          <>
+            <h1>Loading.....</h1>
+          </>
+        );
+    }
+
+  }
 Greeting.propTypes = {
   greeting: PropTypes.string
 };
